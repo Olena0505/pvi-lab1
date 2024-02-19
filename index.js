245896f87b88620e31360ac505748e1261f46d76
Olena0505 <olenakit0505@gmail.com>
2024-02-19 02:27:35 +0200

    Add functionality to 'Add' and 'Delete' buttons with JavaScript

---
+++ index.js
@@ -0,0 +1,44 @@
+document.getElementById('selectAll').addEventListener('change', function() {
+    const checkboxes = document.querySelectorAll('#studentsTable tbody input[type="checkbox"]');
+   
+    checkboxes.forEach(function(checkbox) {
+        checkbox.checked = this.checked;
+    }, this);
+});
+
+document.getElementById('addBtn').onclick = function(){
+    const newRow = document.createElement('tr');
+
+    newRow.innerHTML = `
+        <th><input type="checkbox"></th>
+        <td>KN-21</td>
+        <td>John Smith</td>
+        <td>M</td>
+        <td>11.05.2004</td>
+        <td><span class="inactive-dot"></span></td>
+        <td>
+            <button class="editRowBtn">
+                <i class="fa-solid fa-pencil"></i>
+            </button>
+            <button class="deleteRowBtn">
+                <i class="fa-solid fa-xmark fa-lg"></i>
+            </button>
+        </td>
+    `;
+
+    newRow.classList.add('tableRow');
+    document.getElementById('tableBody').appendChild(newRow);
+
+    const newDeleteButton = newRow.querySelector('.deleteRowBtn');
+    addDeleteListener(newDeleteButton);
+}
+
+let deleteButtons = document.querySelectorAll('.deleteRowBtn');
+deleteButtons.forEach(button => addDeleteListener(button));
+
+function addDeleteListener(button) {
+    button.addEventListener('click', function() {
+        const row = button.closest('tr');
+        row.remove();
+    });
+}
